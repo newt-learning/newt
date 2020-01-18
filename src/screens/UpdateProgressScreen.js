3c5dae7afe940f750efa3af3eb3862878c355c4a
Nehal Udyavar <nehaludyavar@gmail.com>
2020-01-17 23:55:10 -0800

    Input form UI in Update Progress screen to update pages read

--- src/screens/UpdateProgressScreen.js
+++ src/screens/UpdateProgressScreen.js
@@ -1,12 +1,48 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { ScrollView, Text, StyleSheet, TextInput } from "react-native";
+// Design
+import { SEMIBOLD, FS16 } from "../design/typography";
+import { GRAY_5, GRAY_2 } from "../design/colors";
 
 const UpdateProgressScreen = ({ navigation }) => {
   return (
-    <View>
-      <Text>Update Progress</Text>
-    </View>
+    <ScrollView
+      contentContainerStyle={styles.container}
+      scrollEnabled={false}
+      keyboardShouldPersistTaps="handled"
+    >
+      <Text style={styles.text}>I'm on page</Text>
+      <TextInput
+        style={styles.input}
+        keyboardType="numeric"
+        maxLength={8}
+        autoFocus
+      />
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    marginTop: 20,
+    marginHorizontal: 15
+  },
+  text: {
+    textAlignVertical: "center",
+    fontFamily: SEMIBOLD,
+    fontSize: FS16
+  },
+  input: {
+    fontSize: FS16,
+    backgroundColor: GRAY_5,
+    paddingLeft: 5,
+    borderBottomWidth: 1,
+    borderColor: GRAY_2,
+    width: 100,
+    padding: 2,
+    marginLeft: 10
+  }
+});
 
 export default UpdateProgressScreen;
