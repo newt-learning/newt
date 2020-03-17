b7522df342634a17c90d243db48bd2c75e7bb812
Nehal Udyavar <nehaludyavar@gmail.com>
2020-03-17 02:43:54 -0700

    Turns out moment has a startOf time func. lool gr8

--- src/helpers/contextHelpers.js
+++ src/helpers/contextHelpers.js
@@ -34,26 +34,15 @@ export const deleteObjectFromArray = (array, itemId) => {
 // Used in fetching learning stats by period in Stats Context. Returns the start date and end
 // date for a given period (day, week, month, year)
 export const getPeriodStartAndEndDates = period => {
-  // Initialize start and end date variables
-  let startDate, endDate;
   // String to show date format required (used server-side)
   const dateFormatStr = "YYYY-MM-DD";
-  // Current date
-  const today = moment().utc();
 
-  // If the period is "week", then get the start of the week (previous Sunday if it's some)
-  // other day of the week), and end of the week (coming Saturday)
-  if (period === "week") {
-    // Get the day-of-the-week number (Sun = 0 ... Sat = 6). Cloning because original moment object is mutable
-    const todayDayOfWeek = today.clone().day();
-    // Subtract day-of-the-week-number days from today to get start of week
-    startDate = today.clone().subtract(todayDayOfWeek, "days");
-    // Add 6 days to start of the week to get end of the week
-    endDate = startDate.clone().add("6", "days");
-    // Format dates
-    startDate = startDate.format(dateFormatStr);
-    endDate = endDate.format(dateFormatStr);
-  }
+  const startDate = moment()
+    .startOf(period)
+    .format(dateFormatStr);
+  const endDate = moment()
+    .endOf(period)
+    .format(dateFormatStr);
 
   return { startDate, endDate };
 };
