0f682e5f571369d9ab4e9adeaf1c2be33124c7a6
Nehal Udyavar <nehaludyavar@gmail.com>
2020-04-22 03:19:17 -0700

    Add Book and ShelfSelect Screen to content stack

--- src/navigators/AddContentStack.js
+++ src/navigators/AddContentStack.js
@@ -4,6 +4,8 @@ import { NavHeaderTitle } from "../components/Headers";
 // Screens
 import AddContentScreen from "../screens/AddContentScreen";
 import AddBookScreen from "../screens/AddBookScreen";
+import BookScreen from "../screens/BookScreen";
+import ShelfSelectScreen from "../screens/ShelfSelectScreen";
 // Design
 import { OFF_WHITE } from "../design/colors";
 
@@ -23,6 +25,16 @@ const AddContentStack = () => {
         }}
       />
       <Stack.Screen name="Add Book" component={AddBookScreen} />
+      <Stack.Screen
+        name="BookScreen"
+        component={BookScreen}
+        options={{ title: null }}
+      />
+      <Stack.Screen
+        name="ShelfSelect"
+        component={ShelfSelectScreen}
+        options={{ title: null }}
+      />
     </Stack.Navigator>
   );
 };
