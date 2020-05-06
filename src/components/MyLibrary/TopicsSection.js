c0c550e2cd66f1a5afa6c9dc44bdab774a0902c9
Nehal Udyavar <nehaludyavar@gmail.com>
2020-05-05 19:52:29 -0700

    Fetch topics data in Topics section

--- src/components/MyLibrary/TopicsSection.js
+++ src/components/MyLibrary/TopicsSection.js
@@ -1,12 +1,55 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useContext, useEffect } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import _ from "lodash";
+// Context
+import { Context as TopicsContext } from "../../context/TopicsContext";
+// Components
+import Loader from "../shared/Loader";
+// Design
+import { GRAY_2 } from "../../design/colors";
+import { FS16, SEMIBOLD } from "../../design/typography";
 
 const TopicsSection = () => {
+  const {
+    state: { isFetching, items },
+    fetchTopics,
+  } = useContext(TopicsContext);
+
+  // Fetch topics data
+  useEffect(() => {
+    fetchTopics();
+  }, []);
+
+  if (isFetching) {
+    return <Loader />;
+  }
+
+  if (!isFetching && _.isEmpty(items)) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.noDataText}>You haven't created any topics.</Text>
+      </View>
+    );
+  }
+
   return (
-    <View style={{ marginHorizontal: 15 }}>
+    <View style={styles.container}>
       <Text>Topics...</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginHorizontal: 15,
+    marginTop: 15,
+  },
+  noDataText: {
+    fontFamily: SEMIBOLD,
+    fontSize: FS16,
+    color: GRAY_2,
+  },
+});
 
 export default TopicsSection;
