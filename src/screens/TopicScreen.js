3c759388af8b6b3f5d1e73609286dfbb74be2fc0
Nehal Udyavar <nehaludyavar@gmail.com>
2020-05-16 20:19:15 -0700

    Create + init. Instagram like modal for options

--- src/screens/TopicScreen.js
+++ src/screens/TopicScreen.js
@@ -1,17 +1,35 @@
-import React, { useContext } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { Fragment, useContext, useLayoutEffect, useState } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import _ from "lodash";
+import { Feather } from "@expo/vector-icons";
 // Context
 import { Context as ContentContext } from "../context/ContentContext";
 // Components
+import OptionsModal from "../components/shared/OptionsModal";
 import ContentList from "../components/ContentList";
 // Design
 import { SEMIBOLD, FS16 } from "../design/typography";
-import { GRAY_2 } from "../design/colors";
+import { OFF_BLACK, GRAY_2 } from "../design/colors";
 
-const TopicScreen = ({ route }) => {
+const TopicScreen = ({ route, navigation }) => {
   const { topicInfo } = route.params;
   const { state: contentState } = useContext(ContentContext);
+  // Initialize modal visibility
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  // Add the 3-dot options icon which opens the modal on the right side of the header
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity
+          style={{ marginRight: 15 }}
+          onPress={() => setIsModalVisible(true)}
+        >
+          <Feather name="more-horizontal" color={OFF_BLACK} size={24} />
+        </TouchableOpacity>
+      ),
+    });
+  });
 
   // If the topic has no content saved, show no content message
   if (_.isEmpty(topicInfo.content)) {
@@ -33,7 +51,29 @@ const TopicScreen = ({ route }) => {
     return _.find(contentState.items, { _id: contentId });
   });
 
-  return <ContentList data={contentData} />;
+  // List of buttons in the options modal
+  const modalOptions = [
+    {
+      title: "Edit",
+      onPress: () => console.log("Edit"),
+    },
+    {
+      title: "Delete",
+      onPress: () => console.log("Delete"),
+      isDelete: true,
+    },
+  ];
+
+  return (
+    <View style={{ flex: 1 }}>
+      <ContentList data={contentData} />
+      <OptionsModal
+        isVisible={isModalVisible}
+        onBackdropPress={() => setIsModalVisible(false)}
+        options={modalOptions}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
