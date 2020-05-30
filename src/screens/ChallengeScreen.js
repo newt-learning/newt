5820697c0d15d98778f69113bf844ca9ec2da28d
Nehal Udyavar <nehaludyavar@gmail.com>
2020-05-30 16:18:55 -0700

    Wire up Options modal to Challenges screen

--- src/screens/ChallengeScreen.js
+++ src/screens/ChallengeScreen.js
@@ -1,4 +1,4 @@
-import React, { useContext, useLayoutEffect } from "react";
+import React, { useState, useContext, useLayoutEffect } from "react";
 import { View, StyleSheet, FlatList } from "react-native";
 import _ from "lodash";
 import { ProgressCircle } from "react-native-svg-charts";
@@ -7,6 +7,7 @@ import { Text as SVGText } from "react-native-svg";
 import Loader from "../components/shared/Loader";
 import ContentListCard from "../components/ContentListCard";
 import MoreOptionsButton from "../components/shared/MoreOptionsButton";
+import OptionsModal from "../components/shared/OptionsModal";
 // Context
 import { Context as ContentContext } from "../context/ContentContext";
 // Design
@@ -16,27 +17,50 @@ import { FS24, BOLD } from "../design/typography";
 import { handleContentNavigation } from "../helpers/screenHelpers";
 
 const ChallengeScreen = ({ navigation, route }) => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
   // Display more options icon on the right side of the header
   useLayoutEffect(() => {
     navigation.setOptions({
       headerRight: () => (
-        <MoreOptionsButton onPress={() => console.log("options")} />
+        <MoreOptionsButton onPress={() => setIsModalVisible(true)} />
       ),
     });
   });
 
+  // Constants for chart
   const STROKE_WIDTH = 14;
 
+  // Get challenge data passed into route
   const { numItemsFinished, totalItems, itemsFinished } = route.params;
   const finishedFraction = `${numItemsFinished} / ${totalItems}`;
 
+  // Get content data to display the books that have been finished
   const {
     state: { items: content, isFetching: contentIsFetching },
   } = useContext(ContentContext);
-
   const finishedBooks = itemsFinished.map((finishedId) =>
     _.find(content, { _id: finishedId })
   );
+
+  // List of buttons in the options modal
+  const modalOptions = [
+    {
+      title: "Edit",
+      onPress: () => {
+        console.log("Edit");
+        setIsModalVisible(false);
+      },
+    },
+    {
+      title: "Delete",
+      onPress: () => {
+        console.log("Delete");
+        setIsModalVisible(true);
+      },
+      isDelete: true,
+    },
+  ];
 
   const ProgressCircleContainer = () => (
     <View style={styles.progressCircleContainer}>
@@ -64,28 +88,35 @@ const ChallengeScreen = ({ navigation, route }) => {
   return contentIsFetching ? (
     <Loader />
   ) : (
-    <FlatList
-      data={finishedBooks}
-      renderItem={({ item, index }) => {
-        let cardStyle = StyleSheet.compose([
-          styles.bookCard,
-          index === 0 && styles.firstBookCard,
-          index === finishedBooks.length - 1 && styles.lastBookCard,
-        ]);
-        return (
-          <ContentListCard
-            title={item.name}
-            authors={item.authors}
-            thumbnailUrl={item.thumbnailUrl}
-            onPress={() => handleContentNavigation(item, navigation)}
-            cardStyle={cardStyle}
-          />
-        );
-      }}
-      keyExtractor={(item) => item._id}
-      contentContainerStyle={styles.booksContainer}
-      ListHeaderComponent={<ProgressCircleContainer />}
-    />
+    <View style={{ flex: 1 }}>
+      <FlatList
+        data={finishedBooks}
+        renderItem={({ item, index }) => {
+          let cardStyle = StyleSheet.compose([
+            styles.bookCard,
+            index === 0 && styles.firstBookCard,
+            index === finishedBooks.length - 1 && styles.lastBookCard,
+          ]);
+          return (
+            <ContentListCard
+              title={item.name}
+              authors={item.authors}
+              thumbnailUrl={item.thumbnailUrl}
+              onPress={() => handleContentNavigation(item, navigation)}
+              cardStyle={cardStyle}
+            />
+          );
+        }}
+        keyExtractor={(item) => item._id}
+        contentContainerStyle={styles.booksContainer}
+        ListHeaderComponent={<ProgressCircleContainer />}
+      />
+      <OptionsModal
+        isVisible={isModalVisible}
+        onBackdropPress={() => setIsModalVisible(false)}
+        options={modalOptions}
+      />
+    </View>
   );
 };
 
