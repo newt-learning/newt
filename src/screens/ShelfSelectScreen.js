23743c932d9662215c965a9c42676b459cb7ec4d
Nehal Udyavar <nehaludyavar@gmail.com>
2020-05-31 20:24:09 -0700

    Fix #27 (reading challenge updated everytime)

--- src/screens/ShelfSelectScreen.js
+++ src/screens/ShelfSelectScreen.js
@@ -109,8 +109,11 @@ const ShelfSelectScreen = ({ navigation, route }) => {
       contentId: newBook._id,
     });
     // Update the reading challenge by adding this book to the finished list
-    // if a challenge exists.
-    addContentToChallenge(newBook._id);
+    // if a challenge exists (if selected shelf is Finished).
+    if (selectedShelf === "Finished Learning") {
+      addContentToChallenge(newBook._id);
+    }
+
     // Fetch all topics to show updates (again, inefficient because multiple
     // updates together doesn't return the updated items, so can't update global
     // state from dispatch)
