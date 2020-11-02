81bf812b2f7c34b5e92405444877c25d6bb44ee9
Nehal Udyavar <nehaludyavar@gmail.com>
2020-11-01 17:49:25 -0800

    Add/remove content from playlist

--- src/screens/AddToPlaylistScreen.js
+++ src/screens/AddToPlaylistScreen.js
@@ -8,7 +8,6 @@ import {
   useRemoveContentFromPlaylists,
 } from "../api/playlists";
 // Context
-import { Context as TopicsContext } from "../context/TopicsContext";
 import { Context as ContentContext } from "../context/ContentContext";
 // Components
 import Loader from "../components/shared/Loader";
@@ -27,24 +26,21 @@ const AddToPlaylistScreen = ({ navigation, route }) => {
   const { contentId, contentPlaylists } = route.params;
 
   const {
-    state: { isFetching, items },
-    fetchTopics,
-    addContentToTopics,
-    removeContentTopics,
-  } = useContext(TopicsContext);
-  const { updateContent } = useContext(ContentContext);
-
+    state: { isFetching },
+    fetchContent,
+    updateContent,
+  } = useContext(ContentContext);
   const {
     data: allPlaylistsData,
     status: allPlaylistsStatus,
   } = useFetchAllPlaylists();
   const [
     addContentToPlaylists,
-    { status: isAddingContentToPlaylist },
+    { status: addingStatus },
   ] = useAddContentToPlaylists();
   const [
     removeContentFromPlaylists,
-    { status: isRemovingContentFromPlaylists },
+    { status: removingStatus },
   ] = useRemoveContentFromPlaylists();
 
   // Initialize empty multi-select list
@@ -56,10 +52,10 @@ const AddToPlaylistScreen = ({ navigation, route }) => {
 
   // This is a little hacky? way (I think, can't find anything cases like this),
   // to fix the bug where the checkboxes were being initialized before the
-  // topics data loaded, thus showing an empty screen. Since hooks have to be
+  // playlists data loaded, thus showing an empty screen. Since hooks have to be
   // top level, they can't be set conditionally or in useEffect, and there can't
   // be the Loader return before as well (throws an error). This way, the
-  // topicsList is initialized again once the data has loaded (or if there's any
+  // playlistsList is initialized again once the data has loaded (or if there's any
   // change).
   useEffect(() => {
     setCheckboxesFromOutside(
@@ -72,47 +68,55 @@ const AddToPlaylistScreen = ({ navigation, route }) => {
 
   console.log(playlistsList);
 
-  if (allPlaylistsStatus === "loading") {
+  if (
+    allPlaylistsStatus === "loading" ||
+    addingStatus === "loading" ||
+    removingStatus === "loading" ||
+    isFetching
+  ) {
     return <Loader />;
   }
 
-  const updateTopicsAndContent = async (topicsList) => {
-    // First filter through the topics list to get only the checked ones, then
+  const updatePlaylistsAndContent = async (playlistsList) => {
+    // First filter through the playlists list to get only the checked ones, then
     // from those objects only take out the ids
-    const selectedTopicsIds = _.filter(topicsList, { checked: true }).map(
+    const selectedPlaylistsIds = _.filter(playlistsList, { checked: true }).map(
       (item) => item._id
     );
 
-    let topicsToAdd = [];
-    let topicsToRemove = [];
+    let playlistsToAdd = [];
+    let playlistsToRemove = [];
 
-    // For each of the topics selected, if they're not in the existing topics,
-    // only then add it to the topicsToAdd array. Only those topics will then
+    // For each of the playlists selected, if they're not in the existing playlists,
+    // only then add it to the playlistsToAdd array. Only those playlists will then
     // have the content added to it to avoid duplication.
-    selectedTopicsIds.forEach((topicId) => {
-      if (!_.includes(contentTopics, topicId)) {
-        topicsToAdd.push(topicId);
+    selectedPlaylistsIds.forEach((playlistId) => {
+      if (!_.includes(contentPlaylists, playlistId)) {
+        playlistsToAdd.push(playlistId);
       }
     });
 
-    // For each of the existing topics, if they're not in the selected topics,
-    // then add it to the topicsToRemove array. It will be used to remove the
-    // topic <==> content associations.
-    contentTopics.forEach((topicId) => {
-      if (!_.includes(selectedTopicsIds, topicId)) {
-        topicsToRemove.push(topicId);
+    // For each of the existing playlists, if they're not in the selected playlists,
+    // then add it to the playlistsToRemove array. It will be used to remove the
+    // playlist <==> content associations.
+    contentPlaylists.forEach((playlistId) => {
+      if (!_.includes(selectedPlaylistsIds, playlistId)) {
+        playlistsToRemove.push(playlistId);
       }
     });
 
-    // Send request to add the content to the newly selected topics, remove
-    // topics that were unselected, and update the content by adding the topics
+    // Send request to add the content to the newly selected playlists, remove
+    // playlists that were unselected, and update the content by adding the playlists
     // to it
-    updateContent(contentId, { topics: selectedTopicsIds });
-    await addContentToTopics({ topicIds: topicsToAdd, contentId });
-    await removeContentTopics({ topicIds: topicsToRemove, contentId });
-    // Temporarily fetch all topics after updating until I rethink db requests/
-    // db joins and/or complex state management.
-    await fetchTopics();
+    updateContent(contentId, { playlists: selectedPlaylistsIds });
+    await addContentToPlaylists({ playlistIds: playlistsToAdd, contentId });
+    await removeContentFromPlaylists({
+      playlistIds: playlistsToRemove,
+      contentId,
+    });
+    // Temporarily fetch all content after updating until I move ContentContext to
+    // react-query
+    await fetchContent();
   };
 
   // Display message and button to go to Create Playlist screen when there are no
@@ -146,7 +150,7 @@ const AddToPlaylistScreen = ({ navigation, route }) => {
         <ActionButton
           title="Confirm"
           onPress={() => {
-            updateTopicsAndContent(playlistsList);
+            updatePlaylistsAndContent(playlistsList);
             navigation.goBack();
           }}
         />
